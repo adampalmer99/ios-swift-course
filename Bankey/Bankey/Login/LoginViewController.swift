caf23f446fe962fb3681c75db45a3db283504ec9
Adam Palmer <acpalmer1011@gmail.com>
2024-11-19 09:59:52 +0000

    created login view

---
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Adam Palmer on 18/11/2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+    
+    extension LoginViewController {
+        
+        private func style() {
+            loginView.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        private func layout() {
+            view.addSubview(loginView)
+            
+            NSLayoutConstraint.activate([
+                loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+                view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+            ])
+        }
+    }
