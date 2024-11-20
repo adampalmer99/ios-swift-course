2c4b30f4cd3a68bb2bdf3d89bb214d3d1ce4834c
Adam Palmer <acpalmer1011@gmail.com>
2024-11-20 18:39:11 +0000

    imported images, set up stackview for onboarding view controller

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -18,8 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
         //window?.rootViewController = LoginViewController()
-        window?.rootViewController = OnboardingContainerViewController()
+        //window?.rootViewController = OnboardingContainerViewController()
+        window?.rootViewController = OnboardingViewController()
 
+        
         
         return true
     }
