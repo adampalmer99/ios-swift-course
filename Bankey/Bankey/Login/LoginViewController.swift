068ed215329accd07ba2be454e56798dfde333e6
Adam Palmer <acpalmer1011@gmail.com>
2024-11-20 10:45:22 +0000

    added title and subtitle labels to stackview

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
     let errorMessageLabel = UILabel()
@@ -33,6 +35,19 @@ class LoginViewController: UIViewController {
         private func style() {
             loginView.translatesAutoresizingMaskIntoConstraints = false
             
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            titleLabel.textAlignment = .center
+            titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+            titleLabel.adjustsFontForContentSizeCategory = true
+            titleLabel.text = "Bankey"
+
+            subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+            subtitleLabel.textAlignment = .center
+            subtitleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+            subtitleLabel.adjustsFontForContentSizeCategory = true
+            subtitleLabel.numberOfLines = 0
+            subtitleLabel.text = "Your premium source for all things banking!"
+            
             signInButton.translatesAutoresizingMaskIntoConstraints = false
             signInButton.configuration = .filled()
             signInButton.configuration?.imagePadding = 8 // for indicator spacing
@@ -48,8 +63,11 @@ class LoginViewController: UIViewController {
         
         private func layout() {
             view.addSubview(loginView)
+            view.addSubview(titleLabel)
+            view.addSubview(subtitleLabel)
             view.addSubview(signInButton)
             view.addSubview(errorMessageLabel)
+            
             
             // LoginView
             NSLayoutConstraint.activate([
@@ -57,6 +75,20 @@ class LoginViewController: UIViewController {
                 loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
                 view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
             ])
+            
+            // title
+            NSLayoutConstraint.activate([
+                subtitleLabel.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 3),
+                titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+            
+            // subtitle
+            NSLayoutConstraint.activate([
+                loginView.topAnchor.constraint(equalToSystemSpacingBelow: subtitleLabel.bottomAnchor, multiplier: 3),
+                        subtitleLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+                        subtitleLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+            ])
+            
             
             // Button
             NSLayoutConstraint.activate([
